eb80db1ade11935a5010416b79e65ac483c2476f
mma-lab <you@example.com>
2022-06-17 17:29:31 +0900

    tests

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,5 @@
+python sgr_main.py -s 500 -e Carrier-v0 --save_to results/Carrier-v0/run_alt_1 --cpu 12 --pop 128
 python sgr_main.py -s 600 -e UpStepper-v0 --save_to results/UpStepper-v0/run_alt_1 --cpu 12 --pop 128
-python sgr_main.py -s 500 -e Carrier-v1 --save_to results/Carrier-v1/run_alt_1 --cpu 12 --pop 128
 python sgr_main.py -s 300 -e Thrower-v0 --save_to results/Thrower-v0/run_alt_1 --cpu 12 --pop 128
 python sgr_main.py -s 600 -e AreaMinimizer-v0 --save_to results/AreaMinimizer-v0/run_alt_1 --cpu 12 --pop 128
 python sgr_main.py -s 400 -e Climber-v2 --save_to results/Climber-v2/run_alt_1 --cpu 12 --pop 128
@@ -8,7 +8,7 @@ python sgr_main.py -s 600 -e Balancer-v1 --save_to results/Balancer-v1/run_alt_1
 python sgr_main.py -s 500 -e Walker-v0 --save_to results/Walker-v0/run_alt_1 --cpu 12 --pop 128
 
 ############################################################################################################################################################
-python sgr_main.py -s 500 -e Carrier-v1 --save_to results/Carrier-v1/run_alt_2 --cpu 12 --pop 128
+python sgr_main.py -s 500 -e Carrier-v0 --save_to results/Carrier-v0/run_alt_2 --cpu 12 --pop 128
 python sgr_main.py -s 300 -e Thrower-v0 --save_to results/Thrower-v0/run_alt_2 --cpu 12 --pop 128
 python sgr_main.py -s 600 -e AreaMinimizer-v0 --save_to results/AreaMinimizer-v0/run_alt_2 --cpu 12 --pop 128
 python sgr_main.py -s 500 -e Walker-v0 --save_to results/Walker-v0/run_alt_2 --cpu 12 --pop 128
