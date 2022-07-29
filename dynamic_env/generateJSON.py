a835c321a004433c5e1c3a3e14d5596713a491f4
k1 <fhktanaka@gmail.com>
2022-07-29 14:59:52 +0900

    new terrain generation

--- dynamic_env/generateJSON.py
+++ dynamic_env/generateJSON.py
@@ -9,6 +9,8 @@ HORI_VX = 3
 VERT_VX = 4
 FIXED_VX = 5
 
+STARTING_ZONE = 12
+
 def base_json(width, height):
     env_json = {
         "grid_width": width,
@@ -17,33 +19,41 @@ def base_json(width, height):
     }
     return env_json
 
-def generate_env(width, height, barrier_interval, barrier_height_prob):
+def generate_env(width, height, barrier_height, barrier_prob):
     env = np.zeros((height, width))
     env[0][:] = FIXED_VX # building the ground
-    for j in range(12, width, barrier_interval):
-        r = np.random.random()
-        h = 1
-        acc_prob = 0
-        for prob in barrier_height_prob:
-            acc_prob += prob
-            if r <= acc_prob:
-                break
-            h += 1
-        for i in range(0, h):
+    previous_height = 1
+    for j in range(STARTING_ZONE, width):
+        r = np.random.choice(barrier_height, 1, p=barrier_prob)
+        h = np.clip(previous_height + r, 1, height)
+        for i in range(0, int(h)):
             env[i][j] = FIXED_VX
+        previous_height = h
     return env
 
 def ij_to_index(i, j, width):
     return j + i*width
 
+def add_neighbors(i, j, env_vals, width, height):
+    neighbors = []
+    if j > 0 and env_vals[i][j-1] != EMPTY_VX:
+        neighbors.append(ij_to_index(i, j-1, width))
+    if j < width-1 and env_vals[i][j+1] != EMPTY_VX:
+        neighbors.append(ij_to_index(i, j+1, width))
+    if i > 0 and env_vals[i-1][j] != EMPTY_VX:
+        neighbors.append(ij_to_index(i-1, j, width))
+    if i < height-1 and env_vals[i+1][j] != EMPTY_VX:
+        neighbors.append(ij_to_index(i+1, j, width))
+    return neighbors
 
-def env2json(width=90, height=10, barrier_interval=1, barrier_height_prob=[.3,.4,.3]):
+
+def env2json(width=90, height=10, barrier_height=[-1,0,1], barrier_prob=[.3,.35,.35]):
     ground = {
         "indices": [],
         "types": [],
         "neighbors": {}
     }
-    env_vals = generate_env(width, height, barrier_interval, barrier_height_prob)
+    env_vals = generate_env(width, height, barrier_height, barrier_prob)
     for i in range(height):
         for j in range(width):
             idx = ij_to_index(i, j, width)
@@ -51,16 +61,7 @@ def env2json(width=90, height=10, barrier_interval=1, barrier_height_prob=[.3,.4
             if vx_type != EMPTY_VX:
                 ground["indices"].append(idx)
                 ground["types"].append(vx_type)
-                neighbors = []
-                if j > 0 and env_vals[i][j-1] != EMPTY_VX:
-                    neighbors.append(ij_to_index(i, j-1, width))
-                if j < width-1 and env_vals[i][j+1] != EMPTY_VX:
-                    neighbors.append(ij_to_index(i, j+1, width))
-                if i > 0 and env_vals[i-1][j] != EMPTY_VX:
-                    neighbors.append(ij_to_index(i-1, j, width))
-                if i < height-1 and env_vals[i+1][j] != EMPTY_VX:
-                    neighbors.append(ij_to_index(i+1, j, width))
-                ground["neighbors"][idx] = neighbors
+                ground["neighbors"][idx] = add_neighbors(i, j, env_vals, width, height)
 
     env_json = base_json(width, height)
     env_json["objects"]["ground"] = ground
