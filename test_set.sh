ebbb2853206d4afaaa5ea3d8e24808fd005f0c80
mma-lab <you@example.com>
2022-11-19 15:34:56 +0900

    new results with minimal NN and fixed NN

--- test_set.sh
+++ test_set.sh
@@ -1,14 +1,14 @@
-python poet_test.py --substrate 3d -c configs/poet_sgr.json --save_to 3d_5_mult_out_1 | tee mult_out_result_1.txt
+# python poet_test.py --substrate 3d -c configs/poet_sgr.json --save_to 3d_5_mult_out_1 | tee mult_out_3D_result_1.txt
 
-python poet_test.py --substrate cppn -c configs/poet_sgr.json --save_to cppn_5_mult_out_1 | tee mult_out_result_1.txt
-
-
-python poet_test.py --substrate 3d -c configs/poet_sgr.json --save_to 3d_5_mult_out_2 | tee mult_out_result_2.txt
-
-python poet_test.py --substrate cppn -c configs/poet_sgr.json --save_to cppn_5_mult_out_2 | tee mult_out_result_2.txt
+# python poet_test.py --substrate cppn -c configs/poet_sgr.json --save_to cppn_5_mult_out_1 | tee mult_out_cppn_result_1.txt
 
 
-python poet_test.py --substrate 3d -c configs/poet_sgr.json --save_to 3d_5_mult_out_3 | tee mult_out_result_3.txt
+python poet_test.py --substrate 3d -c configs/poet_sgr.json --save_to 3d_5_mult_out_2 | tee mult_out_3D_result_2.txt
 
-python poet_test.py --substrate cppn -c configs/poet_sgr.json --save_to cppn_5_mult_out_3 | tee mult_out_result_3.txt
+python poet_test.py --substrate cppn -c configs/poet_sgr.json --save_to cppn_5_mult_out_2 | tee mult_out_cppn_result_2.txt
+
+
+python poet_test.py --substrate 3d -c configs/poet_sgr.json --save_to 3d_5_mult_out_3 | tee mult_out_3D_result_3.txt
+
+python poet_test.py --substrate cppn -c configs/poet_sgr.json --save_to cppn_5_mult_out_3 | tee mult_out_cppn_result_3.txt
 
