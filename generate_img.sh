78b87b55c6f1d7b0520e4b03f3cb4874dbe15859
fhtanaka <fhktanaka@gmail.com>
2023-02-19 17:57:55 +0900

    better image generator

---
+++ generate_img.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -a arr=("snk_cppn" "snk_3d" "random_loc_3d" "random_loc_cppn")
+
+# Set the directory path
+dir_path="island_cp/"
+
+# Loop over the files in the directory
+for dir in "${arr[@]}"
+do
+    for file in "${dir_path}${dir}"/*
+    do
+    # Check if the file is a regular file
+    if [[ -f "$file" ]]; then
+        # Call the Python function with the file as an argument
+        python generate_imgs.py "${file}"
+    fi
+    done
+done
